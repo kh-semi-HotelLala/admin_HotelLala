ed51e8a12c70de8accb65445e83d8cb302ab2648
sayee88 <dkdbss79@naver.com>
2022-06-13 09:24:56 +0900

    index 수정, reservation 쪽 수정

--- src/main/java/edu/kh/hotellala/reservation/model/service/ReserveService.java
+++ src/main/java/edu/kh/hotellala/reservation/model/service/ReserveService.java
@@ -2,10 +2,38 @@ package edu.kh.hotellala.reservation.model.service;
 
 import static edu.kh.hotellala.common.JDBCTemplate.*;
 
+import java.sql.Connection;
+import java.util.Map;
+
 import edu.kh.hotellala.reservation.model.dao.ReserveDAO;
 
 public class ReserveService {
 	
 	private ReserveDAO dao = new ReserveDAO();
+
+	
+	
+	/**
+	 * 게시글 목록 조회	Service
+	 * @param cp
+	 * @return map
+	 * @throws Exception
+	 */
+	public Map<String, Object> selectReserveList(int cp) throws Exception {
+		
+		Connection conn = getConnection();
+		
+		// 현재 게시판 전체 게시글 수 조회 DAO 호출
+		
+		// 전체 게시글 수 + 현재 페이지 이용해 페이지네이션 객체 생성
+		
+		// 게시글 목록 조회
+		
+		// Map 객체를 생성하여 1,2,3 결과 객체를 모두 저장
+		
+		close(conn);
+		
+		return null;
+	}
 
 }
--- src/main/webapp/resources/js/reservation/room.js
+++ src/main/webapp/resources/js/reservation/room.js
@@ -1,15 +1,46 @@
 // 모달창
-const modal = document.getElementById("modal");
-const modalBtn = document.querySelectorAll(".reserve-detail");
-const closeBtn = modal.querySelector(".modal-close");
 
-modalBtn.addEventListener("click", function(){
+(function(){
+    const reserveDetail = document.getElementsByClassName("reserve-detail");
+    
+    if(reserveDetail.length > 0){
+        const modal = document.getElementById("modal");
+        const modalBtn = document.getElementById("modal-btn");
+        const closeBtn = modal.querySelector(".modal-close");
 
-    modal.style.display = "flex";
+        for(let rd of reserveDetail){
+            rd.addEventListener("click", function(){
+                modal.classList.toggle('show');
+            });
+        }
 
-});
+        closeBtn.addEventListener("click", function(){
+            modal.classList.toggle('hide');
+            setTimeout(function(){
+                modal.classList.toggle('hide');
+                modal.classList.toggle('show');
+            },450);
+        });
 
-closeBtn.addEventListener("click", function(){
-    modal.style.display = "none";
-});
+        modalBtn.addEventListener("click", function(){
+            modal.classList.toggle("hide");
+            setTimeout(function(){
+                modal.classList.toggle('hide');
+                modal.classList.toggle('show');
+            })
+        });
+
+    }
+
+})();
+
+// modalBtn.addEventListener("click", function(){
+
+//     modal.style.display = "flex";
+
+// });
+
+// closeBtn.addEventListener("click", function(){
+//     modal.style.display = "none";
+// });
 
