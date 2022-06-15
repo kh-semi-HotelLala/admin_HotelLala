806ff4a26556b13beb5d9bb24910ac8cbbd0edc1
sayee88 <dkdbss79@naver.com>
2022-06-15 23:11:26 +0900

    한번 더 커밋

--- src/main/java/edu/kh/hotellala/reservation/model/service/ReserveService.java
+++ src/main/java/edu/kh/hotellala/reservation/model/service/ReserveService.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 
 import edu.kh.hotellala.reservation.model.dao.ReserveDAO;
-import edu.kh.hotellala.reservation.model.vo.Payment;
 import edu.kh.hotellala.reservation.model.vo.Reservation;
 
 public class ReserveService {
